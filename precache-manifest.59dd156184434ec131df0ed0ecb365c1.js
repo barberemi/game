0760e3d18741ccf6e0b7392e9cbd66dcdeac0d46
Barbé <barbe.remi25@gmail.com>
2020-10-07 19:02:50 +0200

    Updates

---
+++ precache-manifest.59dd156184434ec131df0ed0ecb365c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6789c65978d614248b31523dd6ee8ce6",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "f98609f27a7c6ab8e912",
+    "url": "/game/static/css/main.d1e0996e.chunk.css"
+  },
+  {
+    "revision": "9195b528228a3a12502a",
+    "url": "/game/static/js/2.d575295d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game/static/js/2.d575295d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98609f27a7c6ab8e912",
+    "url": "/game/static/js/main.b5c507ff.chunk.js"
+  },
+  {
+    "revision": "be523661599ed65828e4",
+    "url": "/game/static/js/runtime-main.eb6a8fc6.js"
+  }
+]);
